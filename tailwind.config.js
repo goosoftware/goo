7004a528ea3a2868196b5f0fbdeceefd24dc22e9
John Rees <john@bitsushi.com>
2021-06-03 09:57:13 +0100

    add nightwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,15 @@ module.exports = {
   // purge: ["./src/renderer/**/*.{html,js,ts,jsx,tsx}"],
   purge: ["src/**/*.tsx", "src/**/*.css"],
   darkMode: "class", // or 'media' or 'class'
+  darkMode: "class",
   theme: {
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    // require("nightwind")
+  ],
 };
