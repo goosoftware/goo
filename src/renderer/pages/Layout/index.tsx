cd6b2353af02437107fcfbdd4df33a1fb8390dcb
John Rees <john@bitsushi.com>
2021-06-04 19:03:48 +0100

    fix svg

--- src/renderer/pages/Layout/index.tsx
+++ src/renderer/pages/Layout/index.tsx
@@ -383,9 +383,9 @@ export default function Layout() {
                             viewBox="0 0 24 24"
                             fill="none"
                             stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             className="feather feather-activity mr-2"
                           >
                             <polyline points="22 12 18 12 15 21 9 3 6 12 2 12"></polyline>
