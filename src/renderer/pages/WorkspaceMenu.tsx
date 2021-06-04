53698fb2e08c379eb05b8158aa6832ffc0a71860
John Rees <john@bitsushi.com>
2021-06-04 19:58:30 +0100

    Update WorkspaceMenu.tsx

--- src/renderer/pages/WorkspaceMenu.tsx
+++ src/renderer/pages/WorkspaceMenu.tsx
@@ -1,16 +1,77 @@
 import { observer } from "mobx-react-lite";
 import React from "react";
-import { Link } from "react-router-dom";
 import { store } from "../models/Store";
 
-const WorkspaceMenu = observer(() => (
-  <ul>
-    {store.sortedWorkspaces.map((ws) => (
-      <li key={ws.id}>
-        <Link to={`/workspaces/${ws.id}`}>{ws.id}</Link>
-      </li>
-    ))}
-  </ul>
-));
-
-export default WorkspaceMenu;
+const WorkspaceMenu = () => (
+  <div className="py-6">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+      <h1 className="text-2xl font-semibold text-gray-900">
+        Anchor Workspaces
+      </h1>
+    </div>
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+      <div className="flex flex-col">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="shadow overflow-hidden sm:rounded-lg">
+              <table className="min-w-full">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Path
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      State
+                    </th>
+                    <th scope="col" className="relative px-6 py-3">
+                      <span className="sr-only">Edit</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {store.sortedWorkspaces.map((workspace, idx) => (
+                    <tr
+                      key={workspace.name}
+                      className={idx % 2 === 0 ? "bg-white" : "bg-gray-50"}
+                    >
+                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        {workspace.name}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        {workspace.path}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        {workspace.state}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                        <a
+                          href="#"
+                          className="text-indigo-600 hover:text-indigo-900"
+                        >
+                          Edit
+                        </a>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+export default observer(WorkspaceMenu);
