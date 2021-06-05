28e7e91513b58581f93de884d31ae93577d35868
John Rees <john@bitsushi.com>
2021-06-05 09:58:29 +0100

    localnet > local

--- src/renderer/pages/Layout/index.tsx
+++ src/renderer/pages/Layout/index.tsx
@@ -268,7 +268,7 @@ export const Sidebar = observer(() => {
                         >
                           <polyline points="22 12 18 12 15 21 9 3 6 12 2 12"></polyline>
                         </svg>
-                        localnet
+                        local
                       </span>
                     )}
                   </a>
