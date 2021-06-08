f1477c4aee00013c2373fd38b0abf6a2337d03c4
John Rees <john@bitsushi.com>
2021-06-08 20:41:53 +0100

    attempted to load nodes subdirs automatically, no joy

--- src/renderer/nodes/index.ts
+++ src/renderer/nodes/index.ts
@@ -1,14 +1,25 @@
-// TODO: this doesn't load when you'd expect because esbuild is strange
-// require("litegraph.js").LiteGraph.clearRegisteredTypes();
-
-// TODO import these automatically and register them with LGraph here
+// TODO: esbuild doesn't like doing this instead
+//
+// LiteGraph.clearRegisteredTypes();
+//
+// readdirSync(resolve("./src/renderer/nodes"), {
+//   withFileTypes: true,
+// })
+//   .filter((d) => d.isDirectory())
+//   .map((d) => d.name)
+//   .filter((n) => !n.startsWith("."))
+//   .map((x) => {
+//     console.log(resolve(`./src/renderer/nodes/${x}`));
+//     return x;
+//   })
+//   .forEach((n) => require(resolve(n)));
 
 import "./anchor";
 import "./arweave";
 import "./charts";
 import "./coingecko";
 import "./crypto";
-// import "./goo";
+import "./index.ts";
 import "./mangomarkets";
 import "./metaplex";
 import "./psyoptions";
