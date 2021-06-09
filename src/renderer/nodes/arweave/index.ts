a78608f3b79b7e5a6bdff2f354a97433aa388a11
John Rees <john@bitsushi.com>
2021-06-09 20:02:51 +0100

    use run in arweave node

--- src/renderer/nodes/arweave/index.ts
+++ src/renderer/nodes/arweave/index.ts
@@ -1,34 +1,32 @@
 import { GooNode } from "@goosoftware/goo";
 import { LiteGraph } from "litegraph.js";
+
 class ArweaveNode extends GooNode {
   static title_color = "#BBB";
 }
 
 class StoragePrice extends ArweaveNode {
   title = "arweave / storage price";
-  private bytes: Number;
-  private price: Number;
 
   constructor() {
     super();
     this.addInput("bytes", "number");
-    // this.addInput("quoteAsset?", 0 as any);
     this.addOutput("price", 0 as any);
 
     this.onExecute = this.onExecute.bind(this);
   }
 
-  onExecute() {
+  async run() {
     const bytes = this.getInputData(0);
-    if (bytes && this.bytes !== bytes) {
-      this.bytes = bytes;
-      fetch(`https://arweave.net/price/${parseInt(bytes, 10)}`)
-        .then((res) => res.text())
-        .then((text) => {
-          this.price = Number(text) / 10 ** 12;
-        });
-    }
-    this.setOutputData(0, this.price);
+
+    const int = parseInt(bytes, 10);
+
+    if (!int) throw "no bytes";
+
+    const res = await fetch(`https://arweave.net/price/${int}`);
+    const text = await res.text();
+
+    this.setOutputData(0, Number(text) / 10 ** 12);
   }
 }
 LiteGraph.registerNodeType(`arweave/storagePrice`, StoragePrice);
